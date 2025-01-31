865e3b86527647b3d590c5b5c46087fbc1cdff24
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2025-01-31 23:54:52 +0900

    chore: shadcn/uiの追加インストール

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Form } from "~/components/ui/form";
 import { useForm } from "react-hook-form";
 import z from "zod";
