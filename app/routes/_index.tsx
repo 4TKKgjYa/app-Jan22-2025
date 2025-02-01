272524f2cf8d00ec460b78689ddae810473a9ea2
Naoyuki-Hayasaka <lelivredes5anneaux@gmail.com>
2025-02-01 13:25:08 +0900

    chore: formコンポーネントのインポート文を変更

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,15 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import { Form } from "~/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
